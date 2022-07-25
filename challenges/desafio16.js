c26d5ce20d01edd5c58a5f824ecab9180b7dfb3a
Mayara <mayandronico@msn.com>
2022-07-25 16:22:52 -0300

    Requisito 16

---
+++ challenges/desafio16.js
@@ -0,0 +1,4 @@
+// Challenge 16 
+
+db.produtos.updateMany({ nome: "Big Mac" }, { $currentDate: { ultimaModificacao: true } });
+    db.produtos.find({ ultimaModificacao: { $exists: true } }, { _id: 0, nome: 1 });
