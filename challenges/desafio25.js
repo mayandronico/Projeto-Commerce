90f0346c7671972e8fb4807b9109b309af0d74c6
Mayara <mayandronico@msn.com>
2022-07-25 17:50:50 -0300

    Requisito 25

--- challenges/desafio25.js
+++ challenges/desafio25.js
@@ -1,12 +1,12 @@
 // Challenge 25 
 
-// db.produtos.updateMany({}, {
-//     $push: {
-//         tags: {
-//             $each: ["combo", "tasty"],
-//             $sort: 1,
-//         },
-//     },
-// });
+db.produtos.updateOne({ valoresNutricionais: { 
+        $elemMatch: { 
+    tipo: "sódio",
+     percentual: {
+         $gt: 40,
+} } } }, {
+    $push: { tags: "muito sódio" },
+        });
 
-// db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
+db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
