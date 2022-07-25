8b9c3815e46e64f43dee30f1de2d25ff076391c4
Mayara <mayandronico@msn.com>
2022-07-25 17:14:34 -0300

    Fix:Formatação dos requisitos 8 e 9

---
+++ challenges/desafio25.js
@@ -0,0 +1,12 @@
+// Challenge 25 
+
+// db.produtos.updateMany({}, {
+//     $push: {
+//         tags: {
+//             $each: ["combo", "tasty"],
+//             $sort: 1,
+//         },
+//     },
+// });
+
+// db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,6 +1,6 @@
 // Challenge 9
 
 db.produtos.find(
-  { 'valoresNutricionais.0.quantidade': { $lt: 500 } },
-  { _id: 0, nome: 1 }
+  { "valoresNutricionais.0.quantidade": { $lt: 500 } },
+  { _id: 0, nome: 1 },
 );
